05044071637d5359a70a5d91532abc9f97238e54
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-19 16:22:02 -0700

    Finch model and migration file, updated index view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from operator import mod 
+from pyexpat import model
 
 # Create your models here.
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  region = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -11,18 +12,6 @@ def about(request):
 
 # Mock Data
 
-class Finch: 
-  def __init__(self, name, type, region, age):
-    self.name = name 
-    self.type = type
-    self.region = region
-    self.age = age
-
-finches = [ 
-  Finch('Nilight', 'House Finch', 'North America and Hawaii', 2),
-  Finch('Bianca', 'American Gold Finch', 'Alberta to North Carolina', 3),
-  Finch('Rosa', 'European Goldfinch', 'Europe', 8),
-]
-
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches' : finches, 'page_name': 'Finches' }) 
