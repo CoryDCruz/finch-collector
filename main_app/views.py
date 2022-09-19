c91468e465bef9309bfdc324691c13281dd20956
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-19 14:39:10 -0700

    created main app and begun creating urls

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+  pass
+
+def about(request):
+  pass 
+
+def finches_index(request):
+  pass 
