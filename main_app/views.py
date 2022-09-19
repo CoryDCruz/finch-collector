d47c95cad4c939add94feebc51aa5d37676f7b2a
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-19 15:03:59 -0700

    finch index view and url

--- main_app/views.py
+++ main_app/views.py
@@ -8,5 +8,21 @@ def home(request):
 def about(request):
   return render (request, 'about.html', {'page_name' : 'About'}) 
 
+
+# Mock Data
+
+class Finch: 
+  def __init__(self, name, type, region, age):
+    self.name = name, 
+    self.type = type,
+    self.region = region, 
+    self.age = age
+
+finches = [ 
+  Finch('Nilight', 'House Finch', 'North America and Hawaii', 2),
+  Finch('Bianca', 'American Gold Finch', 'Alberta to North Carolina', 3),
+  Finch('Rosa', 'European Goldfinch', 'Europe', 8)
+]
+
 def finches_index(request):
-  pass 
+  return render(request, 'finches/index.html', { 'finches': finches }) 
