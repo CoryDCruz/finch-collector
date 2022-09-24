33d329dac7768a3006d790e369d8cd320fc6bed0
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-24 12:42:51 -0700

    added user field to finches and ran migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from tkinter import CASCADE
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 MEALS=(
   ("B", "Breakfast"),
@@ -26,6 +27,7 @@ class Finch(models.Model):
   region = models.TextField(max_length=250)
   age = models.IntegerField()
   toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
