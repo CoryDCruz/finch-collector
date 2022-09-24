27a3083a301bf652909e9d57387f2f05b5669c4d
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-24 12:05:57 -0700

    added frontend functionality to add toys to finches

--- main_app/views.py
+++ main_app/views.py
@@ -20,13 +20,14 @@ def finches_index(request):
 def finch_details(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   feeding_form = FeedingForm()
-  toys_finch_doesnt_have = Toy.objects.values_list('id')
-  
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
+
   return render(request, 'finches/detail.html', 
   { 
     'finch': finch, 
     'page_name' : 'Details',
-    'feeding_form' : feeding_form
+    'feeding_form' : feeding_form,
+    'toys': toys_finch_doesnt_have,
   })
 
 def add_feeding(request, finch_id):
@@ -37,6 +38,12 @@ def add_feeding(request, finch_id):
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('details', finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  # Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('details', finch_id)
+
 
 class FinchCreate(CreateView):
   model = Finch
