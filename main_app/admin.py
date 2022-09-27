c73c9c1d906f3b4e841d49b8eac6224dc60b1fac
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-27 16:32:21 -0700

    photo model and s3 upload

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Finch
 from .models import Toy
 from .models import Feeding
+from .models import Photo
 
 # Register your models here.
 admin.site.register(Finch)
 admin.site.register(Toy)
 admin.site.register(Feeding)
+admin.site.register(Photo)
